eaeacba928394dd096c817c306cc453f48f86478
yingguocs22 <guoyingcs@163.com>
2023-04-07 13:24:58 -0700

    update hello

--- src/nav.js
+++ src/nav.js
@@ -6,7 +6,7 @@ function Nav() {
             <span>Dog-Friendly Restaurants</span> ||
             <Link to="/">Home</Link> |
             <Link to="/profile">Profile</Link> |
-            <Link to="/hello">HelloPage</Link> |
+            <Link to="/hello">Hello</Link> |
         </div>
     );
 }
