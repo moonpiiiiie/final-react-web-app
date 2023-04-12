674b9bc6381b621f9c000607634cc8cb3028a27a
yingguocs22 <guoyingcs@163.com>
2023-04-11 19:03:58 -0700

    update navigation bar style

--- src/nav.js
+++ src/nav.js
@@ -17,78 +17,79 @@ function Nav() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     return (
-        <nav className = "navbar navbar-expand-lg navbar-light navigation-bar-color">
-            {/*<a className="navbar-brand" href="#">Dog-Friendly Restaurants</a>*/}
-            {
-                !currentUser && (
-                    <a className="navbar-brand">Dog-Friendly Restaurants</a>
-                )
-            }
-
-            {
-                currentUser && (
-                    <a className="navbar-brand">Hi {currentUser.username}</a>
-                )
-            }
-
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <Link to="/" className={`nav-link ${active === ''?'active':''}`}>Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/search" className={`nav-link ${active === 'search'?'active':''}`}>Search</Link>
-                    </li>
-                    {
-                        currentUser && currentUser.role === "ADMIN" && (
-                            <>
-                                <li className="nav-item">
-                                    <Link to="/admin" className={`nav-link ${active === 'admin'?'active':''}`}>Admin</Link>
-                                </li>
-                            </>
-                        )
-                    }
-                    {
-                        !currentUser && (
-                            <>
-                                <li className="nav-item">
-                                    <Link to="/login" className={`nav-link ${active === 'login'?'active':''}`}>Login</Link>
-                                </li>
-
-                                <li className="nav-item">
-                                    <Link to="/register" className={`nav-link ${active === 'register'?'active':''}`}>Register</Link>
-                                </li>
-                            </>
-                        )
-                    }
-
-                    {
-                        currentUser && (
-                            <>
-                                <li className="nav-item">
-                                    <Link to="/profile" className={`nav-link ${active === 'profile'?'active':''}`}>Profile</Link>
-                                </li>
-                            </>
-                        )
-                    }
-                </ul>
-
+        <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container-fluid">
                 {
-                    currentUser && (
-                        <>
-                            <div className="d-grid gap-2 d-md-flex">
-                                <button className="btn btn-outline-danger my-2 my-sm-0" type="submit" onClick={() => {
-                                    dispatch(logoutThunk());
-                                    // if log out, we will return to the home page
-                                    navigate("/");
-                                }}>Logout</button>
-                            </div>
-                        </>
+                    !currentUser && (
+                        <a className="navbar-brand">Dog-Friendly Restaurants</a>
                     )
                 }
 
+                {
+                    currentUser && (
+                        <a className="navbar-brand">Hi {currentUser.username}</a>
+                    )
+                }
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="nav nav-pills card-header-pills me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link to="/" className={`nav-link ${active === ''?'active':''}`}>Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/search" className={`nav-link ${active === 'search'?'active':''}`}>Search</Link>
+                        </li>
+                        {
+                            currentUser && currentUser.role === "ADMIN" && (
+                                <>
+                                    <li className="nav-item">
+                                        <Link to="/admin" className={`nav-link ${active === 'admin'?'active':''}`}>Admin</Link>
+                                    </li>
+                                </>
+                            )
+                        }
+                        {
+                            !currentUser && (
+                                <>
+                                    <li className="nav-item">
+                                        <Link to="/login" className={`nav-link ${active === 'login'?'active':''}`}>Login</Link>
+                                    </li>
+
+                                    <li className="nav-item">
+                                        <Link to="/register" className={`nav-link ${active === 'register'?'active':''}`}>Register</Link>
+                                    </li>
+                                </>
+                            )
+                        }
+
+                        {
+                            currentUser && (
+                                <>
+                                    <li className="nav-item">
+                                        <Link to="/profile" className={`nav-link ${active === 'profile'?'active':''}`}>Profile</Link>
+                                    </li>
+                                </>
+                            )
+                        }
+                    </ul>
+                    <form className="d-flex" role="search">
+                        {
+                            currentUser && (
+                                <>
+                                    <div className="d-grid gap-2 d-md-flex">
+                                        <button className="btn btn-outline-danger my-2 my-sm-0" type="submit" onClick={() => {
+                                            dispatch(logoutThunk());
+                                            // if log out, we will return to the home page
+                                            navigate("/");
+                                        }}>Logout</button>
+                                    </div>
+                                </>
+                            )
+                        }
+                    </form>
+                </div>
             </div>
         </nav>
+
     );
 }
 
